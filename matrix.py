f3e6a87b14d39138184352cd7ec7c5c0c5850f00
Чекрыжов Аркадий Кириллович <achekryzhov@phoenixit.ru>
2022-04-22 17:29:51 +0300

    sosta'

--- matrix.py
+++ matrix.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from re import S
 import numpy as np
 
 class Matrix(object):
@@ -7,13 +8,24 @@ class Matrix(object):
     deltaRows: np.array
 
     def __init__(self, file: Path) -> None:
-        self.matrix = np.loadtxt(file)
-        self.sumRows = np.array([])
+        self.matrix = np.loadtxt(file, delimiter=', ')
+        self.sumRows = np.sum(self.matrix,axis=1).tolist()
         self.deltaRows = np.array([])
+
+    def delete_cr(self, cr):
+        for i, _ in enumerate(self.matrix):
+            for j, _ in enumerate(self,matrix[i]):
+                if i == cr or j == cr:
+                    self.matrix[i][j] = 0
+        self.sumRows = np.sum(self.matrix,axis=1).tolist()
 
     def print_matrix(self):
         print(f'Матрица:\n {self.matrix}\n\n\n Сумма строк:\n {self.sumRows}\n\n\n\n Непонятная хрень строк:\n {self.deltaRows}')
+
         
 
-matrix = Matrix()
+matrix = Matrix('test50.txt')
+for mat in matrix.matrix:
+    for m in mat:
+        print()
 print(matrix.print_matrix())
