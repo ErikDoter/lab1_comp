b1f2f88bfe279866de0512a03df6ee52fdb9e1d2
Чекрыжов Аркадий Кириллович <achekryzhov@phoenixit.ru>
2022-04-22 17:45:08 +0300

    lezhat'

--- matrix.py
+++ matrix.py
@@ -14,10 +14,31 @@ class Matrix(object):
 
     def delete_cr(self, cr):
         for i, _ in enumerate(self.matrix):
-            for j, _ in enumerate(self,matrix[i]):
+            for j, _ in enumerate(self.matrix[i]):
                 if i == cr or j == cr:
                     self.matrix[i][j] = 0
         self.sumRows = np.sum(self.matrix,axis=1).tolist()
+
+    def get_deltas(self, arr: np.array) -> dict:
+        res = {}
+        for i in arr:
+            dec = 0 #сумма в выделенных стобцах
+            for j in arr:
+                dec = dec + self.matrix[i][j]
+            res[i] = self.sumRows - dec
+        return res
+
+    def min_by_sum(self) -> int:
+        return np.argmin(self.sumRows)
+
+    def get_intesection_row(self, row) -> np.array:
+        res = np.empty((0))
+        for i, _ in self.matrix[row]:
+            if self.matrix[row][i] != 0:
+                res = np.append(res, self.matrix[row][i])
+        
+        return res
+
 
     def print_matrix(self):
         print(f'Матрица:\n {self.matrix}\n\n\n Сумма строк:\n {self.sumRows}\n\n\n\n Непонятная хрень строк:\n {self.deltaRows}')
